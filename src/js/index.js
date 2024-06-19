174314b6b914234cad41882c58349dbe494b99a6
CeliaGallegoGomez <cela17@hotmail.es>
2024-06-19 18:56:20 +0200

    UPDATE

--- src/js/index.js
+++ src/js/index.js
@@ -3,4 +3,5 @@ import "../scss/styles.scss";
 // //Callback --función que ejecuta otra función.
 // //callback y printdata son lo mismo (o por lo menos son intercambiables)
 
+//- Crea una función que reciba un array de números y muestre por consola cada número multiplicado por su índice en el array
 
